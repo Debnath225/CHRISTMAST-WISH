fef8f5225c01597ef38934c2842658c500b0f558
Debnath <166276275+Debnath225@users.noreply.github.com>
2024-12-25 01:51:46 +0530

    Update script.js
    Ok

--- script.js
+++ script.js
@@ -2,18 +2,22 @@ const sBtn = document.getElementById("sBtn");
 const fName = document.getElementById("fName");
 const lName = document.getElementById("lName");
 function addUser(){
+  
 const User=["Debnath Hati","Somnath Hati"];
   if(fName!==0&&lName!==0){
 const givenVal = `fName +lName`;
     User.push()=givenVal;
+    prompt(`MARRY CHRISTMAS givenVal`);
   }
   elseif(fName!==0&&lName===0){
 givenVal = `fName`;
     User.push()=givenVal;
+    prompt(`MARRY CHRISTMAS givenVal`);
   }
   elseif(fName===0&&lName!==0){
 givenVal = `lName`;
     User.push() = givenVal;
+    prompt(`MARRY CHRISTMAS givenVal`);
   }
   else{
     return 0;
